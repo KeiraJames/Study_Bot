a3890b78371a2376c5f96b36d8b4e493dca05086
Keira James <121837616+KeiraJames@users.noreply.github.com>
2025-06-01 20:03:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import os
+from dotenv import load_dotenv # For managing API keys, though not strictly needed for just loading
+
 import streamlit as st
+
+# Import libraries:
+
+from langchain_community.documemt_loaders import PyPDFLoader, DirectoryLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import <embedding model >
+from langchain.vectorstores import <vector model>
+from langchain.llms import <llm model>
+from langchain.chains import RetreivalQA
 
 st.write("Empty App")
